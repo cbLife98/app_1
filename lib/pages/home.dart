633e22648705193288cc09b228a35cd5dd322b0e
Isha <ishagar98@gmail.com>
2018-10-14 09:58:29 +0530

    added

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:app_1/productsManager.dart';
+import 'manage_products.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
+      drawer: new Drawer(
+        child: new Column(
+          children: <Widget>[
+            AppBar(
+              title: new Text("Choose"),
+              automaticallyImplyLeading: false,
+            ),
+            ListTile(
+              leading: new Icon(Icons.location_on),
+              title: new Text("Manage Products"),
+              onTap: () {
+                Navigator.pushReplacement(context, new MaterialPageRoute(
+                    builder: (BuildContext context) => ManageProduct()
+                ));
+              },)
+          ],
+        ),
+      ),
       appBar: new AppBar(
         title: new Text('My App'),
       ),
